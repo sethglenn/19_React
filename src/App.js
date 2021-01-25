d4a8b9d5968518e72774088205aa7d3d61574cb8
Seth Glenn <Sbgees@yahoo.com>
2021-01-24 23:16:36 -0600

    created components foundation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import Table from "./components/Table"
-import employees from "./employees.json";
+// import Table from "./components/table"
+// import employees from "./employees.json";
 
 function App() {
   return (
     <Wrapper>
-      <Title>employees List</Title>
-      <FriendCard
-        name={employees[0].name}
-        image={employees[0].image} 
-        occupation={employees[0].occupation}
-        location={employees[0].location}
-      />
-      <FriendCard
-        name={employees[1].name}
-        image={employees[1].image}
-        occupation={employees[1].occupation}
-        location={employees[1].location}
-      />
-      <FriendCard
-        name={employees[2].name}
-        image={employees[2].image}
-        occupation={employees[2].occupation}
-        location={employees[2].location}
-      />
+      <Title>Employees List</Title>
     </Wrapper>
   );
 }
